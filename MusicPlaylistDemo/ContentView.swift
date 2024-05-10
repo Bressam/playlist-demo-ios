72fe390dd82b58edd0c2fd89fd61722e4812d49f
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2024-05-10 18:22:32 -0300

    created xcode project

---
+++ MusicPlaylistDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicPlaylistDemo
+//
+//  Created by Giovanne Bressam on 10/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicPlaylistDemo/MusicPlaylistDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlaylistDemoApp.swift
+//  MusicPlaylistDemo
+//
+//  Created by Giovanne Bressam on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlaylistDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
