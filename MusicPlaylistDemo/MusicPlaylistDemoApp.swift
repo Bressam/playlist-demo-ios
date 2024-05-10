37633379c805f403f51699ed0f5b7f876744b24c
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2024-05-10 18:43:34 -0300

    Added project basic structures

--- MusicPlaylistDemo/MusicPlaylistDemoApp.swift
+++ MusicPlaylistDemo/MusicPlaylistDemoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MusicPlaylistDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MusicPlaylist()
         }
     }
 }
--- MusicPlaylistDemo/Scenes/MusicPlaylist.swift
+++ MusicPlaylistDemo/Scenes/MusicPlaylist.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MusicPlaylist.swift
 //  MusicPlaylistDemo
 //
 //  Created by Giovanne Bressam on 10/05/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MusicPlaylist: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MusicPlaylist()
 }
